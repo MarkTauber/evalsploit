79611c436d2f8193686b606df29e52da936a79d6
MarkTauber <96663117+MarkTauber@users.noreply.github.com>
2026-04-19 02:48:20 +0300

    Add files via upload
    fixed 0-byte files bug.

--- evalsploit/modules/snippets/dir.php
+++ evalsploit/modules/snippets/dir.php
@@ -4,8 +4,7 @@ foreach ($files2 as $item) {
     $path = $directory."/".$item;
     $stat = stat($path);
     if(is_dir($path)){$type = "[D]";$size = "     ";}else{$type = "[F]";
-        $sizes = log($stat['size'], 1024);
-        $size = str_pad(round(pow(1024, $sizes - floor($sizes)), $precision=0) . array('b', 'kb', 'mb', 'gb', 'tb')[floor($sizes)], 5, " ");
+        $sz=$stat['size'];if($sz>0){$sizes=log($sz,1024);$size=str_pad(round(pow(1024,$sizes-floor($sizes))).[0=>'b',1=>'kb',2=>'mb',3=>'gb',4=>'tb'][floor($sizes)],5," ");}else{$size="0b   ";}
     }
     if(is_readable($path)){$ir = "1";}else{$ir = "0";}
     if(is_writable($path)){$iw = "1";}else{$iw = "0";}
--- evalsploit/modules/snippets/ls.php
+++ evalsploit/modules/snippets/ls.php
@@ -2,10 +2,9 @@ $directory = __DIR_PLACEHOLDER__;
 if(file_exists($directory)  && is_readable($directory) ){
 foreach (new DirectoryIterator($directory) as $fileInfo) {
     if($fileInfo->isDot()) continue;
-    $base = log($fileInfo->getsize(), 1024);
     if($fileInfo->isreadable()){$ir = "1";}else{$ir = "0";}
     if($fileInfo->iswritable()){$iw = "1";}else{$iw = "0";}
-    if(!$fileInfo->isdir()){$if="[F]";$size = str_pad(round(pow(1024, $base - floor($base)), $precision=0) . array('b', 'kb', 'mb', 'gb', 'tb')[floor($base)], 5, " ");}else{$if="[D]"; $size = "     ";}
+    if(!$fileInfo->isdir()){$if="[F]";$sz=$fileInfo->getSize();if($sz>0){$base=log($sz,1024);$size=str_pad(round(pow(1024,$base-floor($base))).[0=>'b',1=>'kb',2=>'mb',3=>'gb',4=>'tb'][floor($base)],5," ");}else{$size="0b   ";}}else{$if="[D]"; $size = "     ";}
     echo  gmdate("[Y-m-d h:i:s]", $fileInfo->getCTime()) . " ". $if. " |" . $ir .":".  $iw .  "| " . $size . " | ". $fileInfo->getFilename() . "\n";
 }
 }else{
