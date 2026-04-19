dfcafabf6d016de59081891d742d62b9ac74c41c
MarkTauber <96663117+MarkTauber@users.noreply.github.com>
2026-04-19 03:44:20 +0300

    Add files via upload
    old PHP versions compatibility

--- evalsploit/modules/snippets/dir.php
+++ evalsploit/modules/snippets/dir.php
@@ -4,7 +4,7 @@ foreach ($files2 as $item) {
     $path = $directory."/".$item;
     $stat = stat($path);
     if(is_dir($path)){$type = "[D]";$size = "     ";}else{$type = "[F]";
-        $sz=$stat['size'];if($sz>0){$sizes=log($sz,1024);$size=str_pad(round(pow(1024,$sizes-floor($sizes))).[0=>'b',1=>'kb',2=>'mb',3=>'gb',4=>'tb'][floor($sizes)],5," ");}else{$size="0b   ";}
+        $sz=$stat['size'];if($sz>0){$sizes=log($sz,1024);$_u=array('b','kb','mb','gb','tb');$size=str_pad(round(pow(1024,$sizes-floor($sizes))).$_u[min(4,(int)floor($sizes))],5," ");}else{$size="0b   ";}
     }
     if(is_readable($path)){$ir = "1";}else{$ir = "0";}
     if(is_writable($path)){$iw = "1";}else{$iw = "0";}
--- evalsploit/modules/snippets/ls.php
+++ evalsploit/modules/snippets/ls.php
@@ -4,7 +4,7 @@ foreach (new DirectoryIterator($directory) as $fileInfo) {
     if($fileInfo->isDot()) continue;
     if($fileInfo->isreadable()){$ir = "1";}else{$ir = "0";}
     if($fileInfo->iswritable()){$iw = "1";}else{$iw = "0";}
-    if(!$fileInfo->isdir()){$if="[F]";$sz=$fileInfo->getSize();if($sz>0){$base=log($sz,1024);$size=str_pad(round(pow(1024,$base-floor($base))).[0=>'b',1=>'kb',2=>'mb',3=>'gb',4=>'tb'][floor($base)],5," ");}else{$size="0b   ";}}else{$if="[D]"; $size = "     ";}
+    if(!$fileInfo->isdir()){$if="[F]";$sz=$fileInfo->getSize();if($sz>0){$base=log($sz,1024);$_u=array('b','kb','mb','gb','tb');$size=str_pad(round(pow(1024,$base-floor($base))).$_u[min(4,(int)floor($base))],5," ");}else{$size="0b   ";}}else{$if="[D]"; $size = "     ";}
     echo  gmdate("[Y-m-d h:i:s]", $fileInfo->getCTime()) . " ". $if. " |" . $ir .":".  $iw .  "| " . $size . " | ". $fileInfo->getFilename() . "\n";
 }
 }else{
